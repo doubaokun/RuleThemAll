5ce2352977bc0837f68b4e9d5fb627c5c277352d
Krzysztof Janosz <janosz.krzysztof@gmail.com>
2015-08-20 12:28:26 +0200

    Refactor WiFi rules

--- core/src/main/scala/sta/parser/triggers/WiFiRules.scala
+++ core/src/main/scala/sta/parser/triggers/WiFiRules.scala
@@ -15,18 +15,16 @@ object WiFiRules extends TriggerParser[WiFi] {
   }
 
   def connection: P[AtomicTrigger[WiFiConnection]] = {
-    lazy val BSSID = MacAddress
-
     lazy val SSID = String.filter(_.length <= 32)
 
-    P("connected" ~ "to" ~ ((BSSID map (v => AtomicTrigger[WiFiConnection] {
+    P("connected" ~ "to" ~ ((MacAddress map (v => AtomicTrigger[WiFiConnection] {
         case WiFiConnection.Connected(_, bssid) => v == bssid
         case _ => false
       })) | (SSID map (v => AtomicTrigger[WiFiConnection] {
         case WiFiConnection.Connected(ssid, _) => v == ssid
         case _ => false
       }))) | ("disconnected" ~ "from" ~ (
-      (BSSID map (v => AtomicTrigger[WiFiConnection] {
+      (MacAddress map (v => AtomicTrigger[WiFiConnection] {
         case WiFiConnection.Disconnected => true
         case WiFiConnection.Connected(_, bssid) => v != bssid
       })) | (SSID map (v => AtomicTrigger[WiFiConnection] {
