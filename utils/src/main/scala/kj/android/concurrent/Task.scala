3cb908d09c407d10e4720846cd4a402424b62357
Krzysztof Janosz <janosz.krzysztof@gmail.com>
2015-08-31 23:13:03 +0200

    Add Task.repeat
    Add Task.repeat that uses CronExpression to spawn repeating task.
    Fix Task.scheduleOnce to include handler function execution.
    Add Task.join fo joining multiple tasks.

--- utils/src/main/scala/kj/android/concurrent/Task.scala
+++ utils/src/main/scala/kj/android/concurrent/Task.scala
@@ -1,7 +1,9 @@
 package kj.android.concurrent
 
+import java.util.Date
 import java.util.concurrent.{FutureTask, ScheduledFuture}
-import scala.concurrent.duration.Duration
+import kj.android.cron.CronExpression
+import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Promise}
 import scala.util.Try
 
@@ -12,6 +14,12 @@ trait Task[T] {
 }
 
 object Task {
+  def join[T](tasks: Seq[Task[T]]): Task[T] = new Task[T] {
+    def cancel(mayInterruptIfRunning: Boolean): Boolean = tasks.forall(_.cancel(mayInterruptIfRunning))
+
+    def run(handler: Try[T] => Unit): Unit = tasks.foreach(_.run(handler))
+  }
+
   def apply[T](body: => T)(implicit ec: ExecutionContext = Implicits.stdExecutionContext): Task[T] =
     new Task[T] {
       private[this] val promise = Promise[T]()
@@ -55,13 +63,40 @@ object Task {
   def scheduleOnce[T](delay: Duration)(body: => T)
     (implicit ec: ScheduledExecutionContext = Implicits.scheduledExecutionContext): Task[T] =
     new Task[T] {
-      private[this] var scheduled: ScheduledFuture[T] = null
+      @volatile private[this] var scheduled: ScheduledFuture[_] = null
 
       def cancel(mayInterruptIfRunning: Boolean): Boolean =
         scheduled != null && scheduled.cancel(mayInterruptIfRunning)
 
       def run(handler: Try[T] => Unit): Unit = {
-        scheduled = Scheduler.scheduleOnce(delay)(body)
+        scheduled = Scheduler.scheduleOnce(delay) {
+          val promise = Promise[T]()
+          promise.tryComplete(Try(body))
+          promise.future.onComplete(handler)
+        }
+      }
+    }
+
+  def repeat[T](expression: CronExpression)(body: => T)
+    (implicit ec: ScheduledExecutionContext = Implicits.scheduledExecutionContext): Task[T] =
+    new Task[T] {
+      @volatile private[this] var scheduled: ScheduledFuture[_] = null
+
+      @inline private def nextDelay: Option[Duration] =
+        expression.nextDate(new Date).map(d => (d.getTime - System.currentTimeMillis()).millis)
+
+      def cancel(mayInterruptIfRunning: Boolean): Boolean =
+        scheduled != null && scheduled.cancel(mayInterruptIfRunning)
+
+      def run(handler: Try[T] => Unit): Unit = {
+        val promise = Promise[T]()
+        nextDelay.foreach(d =>
+          scheduled = Scheduler.scheduleOnce(d) {
+            promise.tryComplete(Try(body))
+            promise.future.onComplete(handler)
+          }
+        )
+        promise.future.onSuccess { case _ => run(handler) }
       }
     }
 }
