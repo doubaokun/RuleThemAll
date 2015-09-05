b2bc685cbda6b62d2afceda06c2122277d8ee093
Krzysztof Janosz <janosz.krzysztof@gmail.com>
2015-09-05 18:22:11 +0200

    Add set brightness and screen timeout actions

--- core/src/main/scala/sta/parser/actions/SetToRules.scala
+++ core/src/main/scala/sta/parser/actions/SetToRules.scala
@@ -1,15 +1,17 @@
 package sta.parser.actions
 
 import fastparse.noApi._
-import sta.model.actions.{ChangeSoundProfile, SetTo}
+import scala.{Int => ScalaInt}
+import sta.model.actions.{SetToSettings, ChangeSoundProfile, SetTo}
 
 object SetToRules extends ActionParser[SetTo] {
   import white._
 
   def rules: Seq[(String, P[SetTo])] = Seq(
-    "sound profile" ->  P(
-      mapParser(ChangeSoundProfile.Mode.namesToValuesMap) map (ChangeSoundProfile(_))
-    )
+    "brightness" -> Percent.map(SetToSettings.brightness),
+    "screen timeout" -> (Duration.map(_.toMillis.toInt) |
+      "never".u.map(_ => ScalaInt.MaxValue)).map(SetToSettings.timeout),
+    "sound profile" -> mapParser(ChangeSoundProfile.Mode.namesToValuesMap).map(ChangeSoundProfile(_))
   )
 
   val Rule: P[SetTo] = {
@@ -17,10 +19,10 @@ object SetToRules extends ActionParser[SetTo] {
       val (str, value) = kv
       val splitted = str.split("\\s+")
       val prefix = splitted.tail.foldLeft(splitted.head: P[Unit]) { _ ~ _ }
-      prefix ~ "to" ~ value
+      prefix ~! "to" ~! P(value)(str)
     }
     val seq = rules
 
-    "set" ~ seq.tail.foldLeft(single(seq.head)) { _ | single(_) }
+    "set" ~! seq.tail.foldLeft(single(seq.head)) { _ | single(_) }
   }
 }
--- core/src/main/scala/sta/parser/actions/TurnOnOffRules.scala
+++ core/src/main/scala/sta/parser/actions/TurnOnOffRules.scala
@@ -6,7 +6,7 @@ import sta.model.actions.TurnOnOffDevice
 object TurnOnOffRules extends ActionParser[TurnOnOffDevice] {
   import white._
 
-  val Rule: P[TurnOnOffDevice] = "turn" ~ (
+  val Rule: P[TurnOnOffDevice] = "turn" ~! (
     "bluetooth".u.map(_ => TurnOnOffDevice.Bluetooth.apply _) |
     "wifi".u.map(_ => TurnOnOffDevice.WiFi.apply _)
   ) ~ ("on".u.map(_=> true) | "off".u.map(_ => false)) map {
