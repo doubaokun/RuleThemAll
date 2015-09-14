df6413b59c53d963c67a0ce914aa8388d9249048
Krzysztof Janosz <janosz.krzysztof@gmail.com>
2015-09-14 23:51:51 +0200

    Add shared user id

--- utils/src/main/scala/kj/android/common/Common.scala
+++ utils/src/main/scala/kj/android/common/Common.scala
@@ -3,13 +3,13 @@ package kj.android.common
 import scala.language.implicitConversions
 import android.content.Intent
 import kj.android.logging.{LogTag, Logger}
+import scala.util.control.NonFatal
 
 trait Common {
   implicit def toRichIntent(intent: Intent): Common.RichIntent = new Common.RichIntent(intent)
 }
 
 object Common {
-
   class RichIntent protected[Common] (private val intent: Intent) extends AnyVal {
     def extra[T] = new ExpandExtra[T](intent)
   }
@@ -18,7 +18,7 @@ object Common {
     def apply(key: String)(implicit logTag: LogTag): T = try {
       Option(intent.getExtras.get(key)).getOrElse(throw new NullPointerException).asInstanceOf[T]
     } catch {
-      case th: Throwable =>
+      case NonFatal(th) =>
         android.util.Log.e(logTag.tag, s"Error during getting key $key from intent", th)
         throw th
     }
@@ -26,10 +26,9 @@ object Common {
     def get(key: String)(implicit logTag: LogTag): Option[T] = try {
       Option(intent.getSerializableExtra(key)).asInstanceOf[Option[T]]
     } catch {
-      case th: Throwable =>
+      case NonFatal(th) =>
         android.util.Log.e(logTag.tag, s"Error during getting key $key from intent", th)
         None
     }
   }
-
 }
